4a446a231d29f30292b4eb83972995044bea3afd
Study Mania <70087773+study-mania@users.noreply.github.com>
2020-08-23 16:51:57 +0530

    Add files via upload
    -- Adding HeapMemoryStatistics code

---
+++ HeapMemory.java
@@ -0,0 +1,14 @@
+
+public class HeapMemory {
+
+	public static void main(String[] args) {
+
+		Runtime r = Runtime.getRuntime();
+		double mb = 1024 * 1024;
+
+		System.out.println("Max Memory : " + r.maxMemory() / mb);
+		System.out.println("Total Memory : " + r.totalMemory() / mb);
+		System.out.println("Free Memory : " + r.freeMemory() / mb);
+		System.out.println("Consumed Memory : " + (r.totalMemory() / mb - r.freeMemory() / mb));
+	}
+}
