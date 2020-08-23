98ca455c589ab83bd37378c91ee5fc6b012c684e
Study Mania <70087773+study-mania@users.noreply.github.com>
2020-08-23 16:57:53 +0530

    Update HeapMemoryStatistics.java

--- HeapMemoryStatistics.java
+++ HeapMemoryStatistics.java
@@ -1,6 +1,7 @@
+/*Code to retrieve the Heap Memory Area Statistics in JVM
+r.maxMemory() retrieves in bytes. We convert bytes to MegaBytes(MB) here*/
 
 public class HeapMemoryStatistics {
-
 	public static void main(String[] args) {
 
 		Runtime r = Runtime.getRuntime();
@@ -12,3 +13,15 @@ public class HeapMemoryStatistics {
 		System.out.println("Consumed Memory : " + (r.totalMemory() / mb - r.freeMemory() / mb));
 	}
 }
+
+/* Sample OUTPUT
+Max Memory : 891.0
+Total Memory : 61.5
+Free Memory : 60.53981018066406
+Consumed Memory : 0.9601898193359375
+---------------------------------------------------------------------------------------------------------------------
+SAMPLE OUTPUT When we run 'java -Xmx512m HeapMemory'  (Here we are setting maximum memory to 512MB)
+Max Memory : 455.5
+Total Memory : 61.5
+Free Memory : 60.539939880371094
+Consumed Memory : 0.9600601196289062 */
